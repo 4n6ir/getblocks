fba60ee36cd84979ea0babac74c3503aa71589e6
John Lukach <hello@lukach.io>
2024-05-26 00:38:29 +0000

    fix

--- getblocks/__main__.py
+++ getblocks/__main__.py
@@ -5,5 +5,5 @@ if sys.version_info >= (3, 8):
     main()
 else:
     python_version = ".".join(map(str, sys.version_info[:3]))
-    print("This utility requires Python >=3.7. Found {}".format(python_version))
+    print("This utility requires Python >=3.8. Found {}".format(python_version))
     sys.exit(1)
